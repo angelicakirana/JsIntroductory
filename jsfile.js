aef5e06536278240b3b13f121e2675566e5c040c
Angelica Kirana <aksd.70@hotmail.com>
2019-09-13 22:47:13 +0700

    ADD html, stylesheet, dan js

--- jsfile.js
+++ jsfile.js
@@ -1,9 +1,3 @@
-
-// var x = function(num1,num2){
-// 	console.log(num1+num2);
-// }
-//  x(1,2);
-
 function showChanges(total){
 	console.log(total);
 	document.getElementById("answer").innerHTML = total;
@@ -32,39 +26,3 @@ function kalkulasi(){
 	showChanges(total);
 
 }
-
-
-
-// function authenticateLogin(){
-// 	var username = document.querySelector('.username');
-// 	console.log(username.value);
-// 	if (username.value == "angelica"){
-// 		var pass = document.querySelector('.pass');
-// 		console.log(pass.value);
-// 		if(pass.value == "anjing123"){
-// 			alert("anda berhasil masuk");
-// 		} else {
-// 			alert("password salah");
-// 		}
-// 	} else {
-// 		alert("username tidak ada");
-// 	}
-// }
-
-
-// operan = "*";
-// var i=0;
-// var len = a.length;
-// while(i<len){
-// 		x=a[i];
-// 		if(x=="*" || x=="+" || x=="-" || x=="/"){
-// 			operan=x;
-// 			break;
-// 		} 
-// 		i++;
-// 	}
-
-// 	in = a.split(operan);
-
-// xx = tambah(in[0], in[1], operan);
-// alert (xx);
